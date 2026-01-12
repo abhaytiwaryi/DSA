cd3c39c71a6915ab4ea48712d76edbadfe13fab5
Abhay Raj <abhayraj702unique@gmail.com>
2026-01-12 11:40:24 +0530

    char printing

---
+++ patternC.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=4;
+
+    for(int i=0;i<n;i++){ // Outer
+        char ch = 'A';
+        for(int j=0; j<n; j++){ //Inner starts -> line starts.
+            cout << ch;
+            ch = ch +1; //65 + 1 ->
+        }cout<<endl;
+    }
+    return 0;
+}
