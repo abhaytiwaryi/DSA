41e0149ee2f1adac6c25eb8e973d938551eb84a3
Abhay Raj <abhayraj702unique@gmail.com>
2026-01-23 04:09:24 +0530

    .

---
+++ arrays/reverse.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void reverse(int arr[], int n) {
+    int start = 0;
+    int end = n-1;
+
+    while(start<=end) {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for(int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout<<endl;
+}
+
+int main() {
+    
+    int arr[5] = {2, 3, 4, 5, 7};
+    int brr[6] = {9, 8, 7, 6, 5, 4};
+
+    reverse(brr, 6);
+    reverse(arr, 5);
+
+    printArray(arr, 5);
+    printArray(brr, 6);
+
+    return 0;
+}
